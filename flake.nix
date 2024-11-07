10416ddff565a1381d7bc1e9eb3baac94691d441
sai <saitheninja@gmail.com>
2024-11-07 17:36:18 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Nix flake Node.js development environment";
+
+  inputs = {
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs-unstable, ... }:
+    let
+      system = "x86_64-linux";
+
+    in
+    {
+      devShells.${system}.default =
+        let
+          pkgs = import nixpkgs-unstable { inherit system; };
+
+        in
+        pkgs.mkShell {
+          packages = [
+            # node 22.10.0
+            pkgs.nodejs_22
+
+            # playwright 1.47.0
+            # version in package.json must match
+            pkgs.playwright-driver.browsers
+
+            # dotenvx 1.14.2
+            # load env variables in a way that playwright can access
+            pkgs.dotenvx
+          ];
+
+          PLAYWRIGHT_BROWSERS_PATH = "${pkgs.playwright-driver.browsers}";
+        };
+    };
+}
