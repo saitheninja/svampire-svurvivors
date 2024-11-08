b54aae37c950465d03f23ada77b26155a7671182
sai <saitheninja@gmail.com>
2024-11-08 18:44:12 +0200

    Planning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div>
+  <h1>Svampire Svurvivors</h1>
+
+  <p>
+    It's like <a
+      href="https://store.steampowered.com/app/1794680/Vampire_Survivors/"
+      class="underline">Vampire Survivors</a
+    >, but built with Svelte, for the web.
+  </p>
+
+  <p>TODO</p>
+  <ul class="list-disc">
+    <li>keyboard & touch controls</li>
+    <li>enemy spawns, waves</li>
+    <li>boss monsters</li>
+    <li>globals: health, score, timer, experience, gold</li>
+    <li>weapons</li>
+    <li>power ups</li>
+    <li>map</li>
+    <li>arrows pointing to power ups</li>
+    <li>map tiles editor</li>
+    <li>camera that follows player</li>
+  </ul>
+</div>
