eca3828b0bbab9a26ec5a75224c51e43bae56b96
sai <saitheninja@gmail.com>
2024-11-09 18:44:55 +0200

    Docs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -46,26 +46,42 @@
 <div>
   <h1>Svampire Svurvivors</h1>
 
-  <p>
-    It's like <a
-      href="https://store.steampowered.com/app/1794680/Vampire_Survivors/"
-      class="underline">Vampire Survivors</a
-    >, but built with Svelte, for the web.
-  </p>
+  <div>
+    <p>
+      It's like <a
+        href="https://store.steampowered.com/app/1794680/Vampire_Survivors/"
+        class="underline">Vampire Survivors</a
+      >, but built with Svelte, for the web.
+    </p>
 
-  <p>TODO</p>
-  <ul class="list-disc">
-    <li>keyboard & touch controls</li>
-    <li>enemy spawns, waves</li>
-    <li>boss monsters</li>
-    <li>globals: health, score, timer, experience, gold</li>
-    <li>weapons</li>
-    <li>power ups</li>
-    <li>map</li>
-    <li>arrows pointing to power ups</li>
-    <li>map tiles editor</li>
-    <li>camera that follows player</li>
-  </ul>
+    <p>I am so funny haha.</p>
+  </div>
+
+  <div>
+    <p>Links:</p>
+
+    <ul>
+      <li><a href="https://hack.sveltesociety.dev/2024/rules">SvelteHack Rules</a></li>
+      <li><a href="https://hack.sveltesociety.dev/2023/winners">SvelteHack Winnners 2013</a></li>
+    </ul>
+  </div>
+
+  <div>
+    <p>TODO</p>
+
+    <ul class="list-disc">
+      <li>keyboard & touch controls</li>
+      <li>enemy spawns, waves</li>
+      <li>boss monsters</li>
+      <li>globals: health, score, timer, experience, gold</li>
+      <li>weapons</li>
+      <li>power ups</li>
+      <li>map</li>
+      <li>arrows pointing to power ups</li>
+      <li>map tiles editor</li>
+      <li>camera that follows player</li>
+    </ul>
+  </div>
 </div>
 
 <form onsubmit={() => window.requestAnimationFrame(gameLoop)}>
